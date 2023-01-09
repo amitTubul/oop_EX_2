934ff8d542a91efb825860c46567f2b69b1e4fe1
GitlerIsrael <99247120+GitlerIsrael@users.noreply.github.com>
2023-01-09 17:35:48 +0200

    Add files via upload

--- src/CustomExecutor.java
+++ src/CustomExecutor.java
@@ -1,3 +1,48 @@
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class CustomExecutor {
+    private PriorityBlockingQueue<Runnable> queue;
+    private ThreadPoolExecutor executor;
+    private int numOfCores;
+    private volatile int maxPriority;
+
+    public CustomExecutor() {
+        this.numOfCores = Runtime.getRuntime().availableProcessors();
+        this.queue = new PriorityBlockingQueue<>();
+        this.executor = new ThreadPoolExecutor(this.numOfCores/2, this.numOfCores-1,
+                300, TimeUnit.MILLISECONDS, queue);
+        this.maxPriority = 0;
+    }
+
+    public <T> Future<T> submit(Task<T> task) {
+        maxPriority = Math.max(maxPriority, task.getPriority());
+        RunnableFuture<T> ftask = new FutureTask(task);
+        executor.execute(ftask);
+        return ftask;
+//        Future<T> result;
+//        try {
+//            result = executor.submit(task);
+//        } catch (NullPointerException e) {
+//            throw new RuntimeException(e);
+//        }
+//        return result;
+    }
+
+    public <T> Future<T> submit(Callable<T> operation, TaskType type) {
+        return submit(Task.createTask(operation, type));
+    }
+
+    public <T> Future<T> submit(Callable<T> operation) {
+        return submit(Task.createTask(operation));
+    }
+
+    public int getCurrentMax() {
+        return maxPriority;
+    }
+
+    public void gracefullyTerminate() {
+        this.executor.shutdown();
+    }
 
 }
--- src/Task.java
+++ src/Task.java
@@ -1,31 +1,51 @@
-class Task<T> implements Comparable<Task<T>> {
+import java.util.concurrent.Callable;
+
+public class Task<T> implements Comparable<Task<T>> {
     private Callable<T> operation;
     private TaskType type;
+    private T result;
 
     private Task(Callable<T> operation, TaskType type) {
         this.operation = operation;
         this.type = type;
+        this.result=null;
     }
 
-    public static <V> Task<V> of(Callable<V> operation, TaskType type) {
+    public static <T> Task<T> createTask(Callable<T> operation, TaskType type) {
         return new Task<>(operation, type);
     }
 
-    private Task(Callable<T> operation) {
-        this.operation = operation;
-    }
-
-    public static <V> Task<V> of(Callable<V> operation) {
-        return new Task<>(operation);
+    public static <V> Task<V> createTask(Callable<V> operation) {
+        return new Task<>(operation, TaskType.OTHER);
     }
 
     public int getPriority() {
         return type.getPriorityValue();
     }
 
+    public void setPriority(int x){
+        type.setPriority(x);
+    }
+
+    public Callable<T> getOperation() {
+        return this.operation;
+    }
+
     public TaskType getType() {
         return type;
     }
+
+//    public T get() {
+//        return this.result;
+//    }
+//
+//    public void run() {
+//        try {
+//            this.result = operation.call();
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
     public T call() throws Exception {
         return operation.call();
