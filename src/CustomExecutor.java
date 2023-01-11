bd4d4019032b0c3386189f6bb63696aec0c219c9
GitlerIsrael <99247120+GitlerIsrael@users.noreply.github.com>
2023-01-11 11:11:37 +0200

    Add files via upload

--- src/CustomExecutor.java
+++ src/CustomExecutor.java
@@ -1,87 +1,137 @@
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 
-public class CustomExecutor {
-    private PriorityBlockingQueue<Runnable> queue;
-    private ThreadPoolExecutor executor;
-    private int numOfCores;
-    private volatile int maxPriority;
 
-    public PriorityBlockingQueue<Runnable> getQueue() {
-        return queue;
-    }
+/**CustomExecutor class extends ThreadPoolExecutor class and can execute Task class object tasks
+ * by their priority.
+ * The CustomExecutor holds those variables:
+ * queue: holds all the waiting tasks.
+ * numOfCores: holds the number of available processors for the program on this computer.
+ * currentMaxPriority: holds the priority of the task with the highest priority for execution.
+ * queuePriorities: holds the number of tasks of each priority that are waiting in queue.
+ */
+public class CustomExecutor extends ThreadPoolExecutor{
+    private static PriorityBlockingQueue<Runnable> queue = new PriorityBlockingQueue<>();
+    private static int numOfCores = Runtime.getRuntime().availableProcessors();
+    private AtomicInteger currentMaxPriority;
+    private AtomicIntegerArray queuePriorities;
 
+    /**
+     * CustomExecutor constructor (Using ThreadPoolExecutor constructor which get the
+     * minimum of threads, maximum of threads, how much time to keep idle thread alive,
+     * time unit for it and a queue for storing tasks.
+     *
+     * We also initialized currentMaxPriority to Integer.MaxValue and a new Array for
+     * holding queued tasks priorities.
+     */
     public CustomExecutor() {
-        this.numOfCores = Runtime.getRuntime().availableProcessors();
-        this.queue = new PriorityBlockingQueue<>();
-//        this.executor = new ThreadPoolExecutor(this.numOfCores/2, this.numOfCores-1,
-//                300, TimeUnit.MILLISECONDS, queue);
-        this.executor = new ThreadPoolExecutor(1,1,
+        super(numOfCores/2,numOfCores-1,
                 300, TimeUnit.MILLISECONDS, queue);
-        this.maxPriority = 0;
+        this.currentMaxPriority = new AtomicInteger(Integer.MAX_VALUE);
+        this.queuePriorities = new AtomicIntegerArray(10);//The priority can be between 1-10.
     }
 
-//    public <T> Future<T> submit(Task<T> task) {
-//        maxPriority = Math.max(maxPriority, task.getPriority());
-//        Future<T> result;
-//        try {
-//            result = (Future<T>) executor.submit(task);
-//        } catch (NullPointerException e) {
-//            throw new RuntimeException(e);
-//        }
-//        return result;
-//    }
+
+    /**
+     * The method submit a Task object to the CustomExecutor for executing it sometime in
+     * the future.
+     * @param task Task class object
+     * @return a FutureTask Object which holds the result of task.
+     * @param <T> generic type of Task object.
+     */
     public <T> Future<T> submit(Task<T> task) {
-        maxPriority = Math.max(maxPriority, task.getPriority());
-        TaskFuture<T> futureTask = new TaskFuture<>(task.getOperation(), task.getType());
-//        queue.add(futureTask);
-        executor.execute(futureTask);
-        return futureTask;
+        queuePriorities.getAndIncrement(task.getPriority());
+        for (int i=0; i<queuePriorities.length(); i++) {
+            if (queuePriorities.get(i) != 0) {
+                currentMaxPriority.set(i);
+                break;
+            }
+        }
+        super.execute(task);
+        return task;
     }
 
 
-
+    /**
+     * The method creates Task object from Callable and Tasktype objects
+     * and submit it to the CustomExecutor.
+     * @param operation a callable operation.
+     * @param type type of task (holds task's priority).
+     * @return call the submit method with Task object)
+     * @param <T> generic type of Task object.
+     */
     public <T> Future<T> submit(Callable<T> operation, TaskType type) {
         return submit(Task.createTask(operation, type));
     }
 
+
+    /**
+     * The method creates Task object from Callable object
+     * and submit it to the CustomExecutor.
+     * @param operation a callable operation.
+     * @return call the submit method with Task object)
+     * @param <T> generic type of Task object.
+     */
     public <T> Future<T> submit(Callable<T> operation) {
         return submit(Task.createTask(operation));
     }
 
-    public int getCurrentMax() {
-        return maxPriority;
+
+    /**
+     * Tasks queue getter
+     * @return queue
+     */
+    public PriorityBlockingQueue<Runnable> getQueue() {
+        return queue;
     }
 
+
+    /**
+     *The method get currentMaxPriority variable.
+     * @return the priority of the task with the highest priority for execution.
+     */
+    public AtomicInteger getCurrentMax() {
+        return currentMaxPriority;
+    }
+
+
+    /**
+     * Method invoked prior to executing the given Runnable in the given thread.
+     * This method is invoked by thread t that will execute task r,
+     * and may be used to re-initialize ThreadLocals, or to perform logging.
+     * This implementation does nothing, but may be customized in subclasses.
+     * Note: To properly nest multiple overridings, subclasses should generally
+     * invoke super.beforeExecute at the end of this method
+     *
+     * We override this method to update currentMaxPriority a moment before a task
+     * get out from the queue. if there are no more tasks in the queue- set
+     * it to 0.
+     *
+     * @param t the thread that will run task {@code r}
+     * @param r the task that will be executed
+     */
+    @Override
+    protected void beforeExecute(Thread t, Runnable r) {
+        queuePriorities.getAndDecrement(((Task<?>)r).getPriority());
+        for (int i = 0; i < queuePriorities.length(); i++) {
+            if (queuePriorities.get(i) != 0) {
+                currentMaxPriority.set(i);
+                break;
+            } else if (i == queuePriorities.length()-1 && queuePriorities.get(0) == 0) {
+                currentMaxPriority.set(0);
+            }
+        }
+        super.beforeExecute(t, r);
+    }
+
+
+    /**
+     * Initiates an orderly shutdown in which previously submitted tasks are executed,
+     * but no new tasks will be accepted. Invocation has no additional effect if already shut down.
+     */
     public void gracefullyTerminate() {
-        this.executor.shutdown();
+        super.shutdown();
     }
 
-    public class TaskFuture<V> extends FutureTask<V> implements Comparable {
-
-        private TaskType type;
-
-        public TaskFuture(Callable<V> operation, TaskType type) {
-            super(operation);
-            this.type = type;
-        }
-
-        public int getPriority() {
-            return type.getPriorityValue();
-        }
-
-        public void setPriority(int x) {
-            type.setPriority(x);
-        }
-
-        public TaskType getType() {
-            return type;
-        }
-
-        @Override
-        public int compareTo(Object other) {
-            return Integer.compare(this.getPriority(), ((TaskFuture<?>)other).getPriority());
-        }
-
-    }
 }
--- src/CustomExecutorTest.java
+++ src/CustomExecutorTest.java
@@ -1,77 +1,12 @@
+import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
-import org.testng.annotations.Test;
 
 import java.util.concurrent.*;
 class Tests {
-    public static final Logger logger = LoggerFactory.getLogger(Tests.class);
-    /**
-     * check if the queue add by priority,
-     * set core and max to be 1, because we need
-     * that task get in the workqueue.
-     *
-     * Print - print the priorities og the queue's tasks
-     * by the order in the queue
-     */
-    @Test
-    public void partialTest() {
-        CustomExecutor customExecutor = new CustomExecutor();
-        for (int i = 0; i < 8; i++) {
-            Callable<String> testIO = () -> {
-                StringBuilder sb = new StringBuilder("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-                return sb.reverse().toString();
-            };
 
-            var reverseTask1 = customExecutor.submit(testIO, TaskType.IO);
-            var task = Task.createTask(()->{
-                int sum = 0;
-                for (int j = 1; j <= 10; j++) {
 
-                    sum += j;
-                }
-                return sum;
-            }, TaskType.COMPUTATIONAL);
-            var sumTask = customExecutor.submit(task);
-            logger.info(()-> "Current maximum priority = " +
-                    customExecutor.getCurrentMax());
-
-            var testMath = customExecutor.submit(() -> {
-
-                return 1000 * Math.pow(1.02, 5);
-            }, TaskType.OTHER);
-
-            Callable<String> testIO2 = () -> {
-                StringBuilder sb = new StringBuilder("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-
-                return sb.reverse().toString();
-            };
-
-            var testt = customExecutor.submit(testIO2, TaskType.IO);
-            logger.info(()-> "Current maximum priority = " +
-                    customExecutor.getCurrentMax());
-
-            final String get1;
-            final double get2;
-            final int get3;
-            System.out.println(customExecutor.getQueue().toString());
-
-            try {
-                get1 = testt.get();
-                get2 = testMath.get();
-                get3 = (int) sumTask.get();
-            } catch (InterruptedException | ExecutionException e) {
-                throw new RuntimeException(e);
-            }
-            logger.info(()-> "Reversed String = " + get1);
-            logger.info(()->String.valueOf("Total Price = " + get2));
-            logger.info(()-> "Current maximum priority = " +
-                    customExecutor.getCurrentMax());
-
-        }
-        customExecutor.gracefullyTerminate();
-    }
 //    public static final Logger logger = LoggerFactory.getLogger(Tests.class);
-//
 //    @Test
 //    public void partialTest() {
 //        CustomExecutor customExecutor = new CustomExecutor();
@@ -82,38 +17,225 @@ class Tests {
 //            }
 //            return sum;
 //        }, TaskType.COMPUTATIONAL);
-//        Future<Integer> sumTask = customExecutor.submit(task);
-//        final int sum;
-//        try {
-//            sum = sumTask.get(1, TimeUnit.MILLISECONDS);
-//        } catch (InterruptedException | ExecutionException | TimeoutException e) {
-//            throw new RuntimeException(e);
-//        }
-//        logger.info(() -> "Sum of 1 through 10 = " + sum);
-//        Callable<Double> callable1 = () -> {
-//            return 1000 * Math.pow(1.02, 5);
-//        };
+//
 //        Callable<String> callable2 = () -> {
 //            StringBuilder sb = new StringBuilder("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 //            return sb.reverse().toString();
 //        };
-//        // var is used to infer the declared type automatically
-//        Future<Double> priceTask = customExecutor.submit(() -> {
-//            return 1000 * Math.pow(1.02, 5);
-//        }, TaskType.COMPUTATIONAL);
-//        Future<String> reverseTask = customExecutor.submit(callable2, TaskType.IO);
-//        final Double totalPrice;
-//        final String reversed;
-//        try {
-//            totalPrice = priceTask.get();
-//            reversed = reverseTask.get();
-//        } catch (InterruptedException | ExecutionException e) {
-//            throw new RuntimeException(e);
+//
+//        for(int i=0; i<100; i++){
+//            customExecutor.submit(task);
+//            customExecutor.submit(callable2, TaskType.IO);
+//            customExecutor.submit(callable2, TaskType.OTHER);
 //        }
-//        logger.info(() -> "Reversed String = " + reversed);
-//        logger.info(() -> String.valueOf("Total Price = " + totalPrice));
-//        logger.info(() -> "Current maximum priority = " +
-//                customExecutor.getCurrentMax());
+//
+//        System.out.println(customExecutor.getQueue().toString());
+
+
+//    /**
+//     * check if the queue add by priority,
+//     * set core and max to be 1, because we need
+//     * that task get in the workqueue.
+//     *
+//     * Print - print the priorities og the queue's tasks
+//     * by the order in the queue
+//     */
+//    @Test
+//    public void partialTest() {
+//        CustomExecutor customExecutor = new CustomExecutor();
+//        for (int i = 0; i < 8; i++) {
+//            Callable<String> testIO = () -> {
+//                StringBuilder sb = new StringBuilder("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+//                return sb.reverse().toString();
+//            };
+//
+//            var reverseTask1 = customExecutor.submit(testIO, TaskType.IO);
+//            var task = Task.createTask(()->{
+//                int sum = 0;
+//                for (int j = 1; j <= 10; j++) {
+//
+//                    sum += j;
+//                }
+//                return sum;
+//            }, TaskType.COMPUTATIONAL);
+//            var sumTask = customExecutor.submit(task);
+//            logger.info(()-> "Current maximum priority = " +
+//                    customExecutor.getCurrentMax());
+//
+//            var testMath = customExecutor.submit(() -> {
+//
+//                return 1000 * Math.pow(1.02, 5);
+//            }, TaskType.OTHER);
+//
+//            Callable<String> testIO2 = () -> {
+//                StringBuilder sb = new StringBuilder("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+//
+//                return sb.reverse().toString();
+//            };
+//
+//            var testt = customExecutor.submit(testIO2, TaskType.IO);
+//            logger.info(()-> "Current maximum priority = " +
+//                    customExecutor.getCurrentMax());
+//
+//            final String get1;
+//            final double get2;
+//            final int get3;
+//            System.out.println(customExecutor.getQueue().toString());
+//
+//            try {
+//                get1 = testt.get();
+//                get2 = testMath.get();
+//                get3 = (int) sumTask.get();
+//            } catch (InterruptedException | ExecutionException e) {
+//                throw new RuntimeException(e);
+//            }
+//            logger.info(()-> "Reversed String = " + get1);
+//            logger.info(()->String.valueOf("Total Price = " + get2));
+//            logger.info(()-> "Current maximum priority = " +
+//                    customExecutor.getCurrentMax());
+//
+//        }
 //        customExecutor.gracefullyTerminate();
 //    }
+
+
+    public static final Logger logger = LoggerFactory.getLogger(Tests.class);
+
+//    @Test
+//    public void partialTest() {
+//        CustomExecutor customExecutor = new CustomExecutor();
+//        for (int i = 0; i < 20; i++) {
+//            Callable<String> testIO = () -> {
+//                StringBuilder sb = new StringBuilder("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+//                return sb.reverse().toString();
+//            };
+//
+//            var reverseTask1 = customExecutor.submit(testIO, TaskType.IO);
+//            var task = Task.createTask(() -> {
+//                int sum = 0;
+//                for (int j = 1; j <= 10; j++) {
+//                    sum += j;
+//                }
+//                return sum;
+//            }, TaskType.COMPUTATIONAL);
+//            var sumTask = customExecutor.submit(task);
+//            var reverseTask2 = customExecutor.submit(testIO, TaskType.IO);
+//            var reverseTask3 = customExecutor.submit(testIO, TaskType.IO);
+//            var reverseTask4 = customExecutor.submit(testIO, TaskType.IO);
+//            var reverseTask5 = customExecutor.submit(testIO, TaskType.IO);
+//            var sumTask1 = customExecutor.submit(task);
+//            var sumTask2 = customExecutor.submit(task);
+//            var sumTask3 = customExecutor.submit(task);
+//            var sumTask4 = customExecutor.submit(task);
+//            var reverseTask6 = customExecutor.submit(testIO, TaskType.IO);
+//            var reverseTask7 = customExecutor.submit(testIO, TaskType.IO);
+//            var reverseTask8 = customExecutor.submit(testIO, TaskType.IO);
+//            var reverseTask9 = customExecutor.submit(testIO, TaskType.IO);
+//            var sumTask6 = customExecutor.submit(task);
+//            var sumTask7 = customExecutor.submit(task);
+//            var sumTask8 = customExecutor.submit(task);
+//            var sumTask9 = customExecutor.submit(task);
+//            var reverseTask10 = customExecutor.submit(testIO, TaskType.IO);
+//            var reverseTask11 = customExecutor.submit(testIO, TaskType.IO);
+//            var reverseTask12 = customExecutor.submit(testIO, TaskType.IO);
+//            var reverseTask13 = customExecutor.submit(testIO, TaskType.IO);
+//            var sumTask10 = customExecutor.submit(task);
+//            var sumTask11 = customExecutor.submit(task);
+//            var sumTask12 = customExecutor.submit(task);
+//            var sumTask13 = customExecutor.submit(task);
+//            var reverseTask14 = customExecutor.submit(testIO, TaskType.IO);
+//            var reverseTask15 = customExecutor.submit(testIO, TaskType.IO);
+//            var reverseTask16 = customExecutor.submit(testIO, TaskType.IO);
+//            var reverseTask17 = customExecutor.submit(testIO, TaskType.IO);
+//            var sumTask14 = customExecutor.submit(task);
+//            var sumTask15 = customExecutor.submit(task);
+//            var sumTask16 = customExecutor.submit(task);
+//            var sumTask17 = customExecutor.submit(task);
+
+
+
+
+//            var reverseTask2 = customExecutor.submit(testIO, TaskType.IO);
+//            var sumTask1 = customExecutor.submit(task);
+//            var reverseTask3 = customExecutor.submit(testIO, TaskType.IO);
+//            var sumTask2 = customExecutor.submit(task);
+//            var reverseTask4 = customExecutor.submit(testIO, TaskType.IO);
+//            var sumTask3 = customExecutor.submit(task);
+//            var reverseTask5 = customExecutor.submit(testIO, TaskType.IO);
+//            var sumTask4 = customExecutor.submit(task);
+
+
+//            var testMath = customExecutor.submit(() -> {
+//                return 1000 * Math.pow(1.02, 5);
+//            }, TaskType.COMPUTATIONAL);
+//
+//            Callable<String> testIO2 = () -> {
+//                StringBuilder sb = new StringBuilder("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+//                return sb.reverse().toString();
+//            };
+//            var testt = customExecutor.submit(testIO2, TaskType.IO);
+//            System.out.println(customExecutor.getQueue().toString());
+//            customExecutor.getCurrentMax();
+//            final String get1;
+//            final double get2;
+//            final int get3;
+//            try {
+//                get1 = testt.get();
+//                get2 = testMath.get();
+//                get3 = (int) sumTask.get();
+//            } catch (InterruptedException | ExecutionException e) {
+//                throw new RuntimeException(e);
+//            }
+//            logger.info(() -> "Reversed String = " + get1);
+//            logger.info(() -> String.valueOf("Total Price = " + get2));
+//            logger.info(() -> "Current maximum priority = " +
+//                    customExecutor.getCurrentMax());
+//        }
+//        customExecutor.gracefullyTerminate();
+
+    @Test
+
+    public void partialTest() {
+        CustomExecutor customExecutor = new CustomExecutor();
+        Task<Integer> task = Task.createTask(() -> {
+            int sum = 0;
+            for (int i = 1; i <= 10; i++) {
+                sum += i;
+            }
+            return sum;
+        }, TaskType.COMPUTATIONAL);
+        Future<Integer> sumTask = customExecutor.submit(task);
+        final int sum;
+        try {
+            sum = sumTask.get(1, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            throw new RuntimeException(e);
+        }
+        logger.info(() -> "Sum of 1 through 10 = " + sum);
+        Callable<Double> callable1 = () -> {
+            return 1000 * Math.pow(1.02, 5);
+        };
+        Callable<String> callable2 = () -> {
+            StringBuilder sb = new StringBuilder("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            return sb.reverse().toString();
+        };
+        // var is used to infer the declared type automatically
+        Future<Double> priceTask = customExecutor.submit(() -> {
+            return 1000 * Math.pow(1.02, 5);
+        }, TaskType.COMPUTATIONAL);
+        Future<String> reverseTask = customExecutor.submit(callable2, TaskType.IO);
+        final Double totalPrice;
+        final String reversed;
+        try {
+            totalPrice = priceTask.get();
+            reversed = reverseTask.get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        logger.info(() -> "Reversed String = " + reversed);
+        logger.info(() -> String.valueOf("Total Price = " + totalPrice));
+        logger.info(() -> "Current maximum priority = " +
+                customExecutor.getCurrentMax());
+        customExecutor.gracefullyTerminate();
+    }
 }
